2ddbf12c36daad029c266682c3116b67b543f4d3
Randyiscoding <reaston@my365.bellevue.edu>
2025-05-03 01:03:28 -0400

    Completed Mod 8 Assignment

--- Module 8/Ch10Exercise21.cpp
+++ Module 8/Ch10Exercise21.cpp
@@ -8,7 +8,22 @@
 #include <iomanip>
 
 using namespace std;
-
+int count_vowels(string input) {
+    /*
+    Function checks each character in the input, looks for each character in the input in the list of vowels
+    returns "npos" if no character is found (characters that are npos are not vowels) and, adds them to a string
+      */
+    string vowels = "0123456789!@#$%^&*()-_=+\|[]{};:/?.>";
+    for (char charc : input) {
+        if (vowels.find(charc) == string::npos) {
+            ;
+        }
+        else {
+            return -1;
+        }
+    }
+    return 0;
+}
 class BankAccount {
     static int ac_numbergen;
     //Random Number generator. credit: Google https://www.google.com/search?q=c%2B%2B+generate+random+12+digit+number&oq=c%2B%2B+generate+random+12+digit+number&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCTEyMzUxajBqN6gCCLACAfEF7V5t0Grwe5E&sourceid=chrome&ie=UTF-8
@@ -57,9 +72,8 @@ class BankAccount {
         }
     void set_ac_interest_rate(int credit_score) {
             if (credit_score <= 500) {ac_interest_rate = .29;}
-            if (credit_score >= 850) {ac_interest_rate = .01;}
-            if (credit_score >= 501 && credit_score <= 849) {
-                ac_interest_rate = .29 - ((0.28/349)*(credit_score - 501));
+            else if (credit_score >= 850) {ac_interest_rate = .01;}
+            else {ac_interest_rate = .29 - ((0.28/349)*(credit_score - 501));
             }
         }
 
@@ -83,26 +97,48 @@ int main() {
     int accountType = 10;
     double deposit = 0.00;
     int credit_score = 0;
-//TODO: Add error Handling
+
+
     for (int i = 0; i < 10; i++) {
         cout << "\nSetting up account #" << i + 1 << endl;
 
         cout << "Enter first name: ";
         cin >> First;
+        while (First.empty() || count_vowels(First) == -1) {
+            cout << "First name cannot be empty or contain special characters. Please re-enter: ";
+            cin >> First;
+        }
         cout << "Enter last name: ";
         cin >> last;
+        while (last.empty() || count_vowels(last) == -1) {
+            cout << "First name cannot be empty or contain special characters. Please re-enter: ";
+            cin >> last;
+        }
         cout << "Enter account type (0 for Checking, 1 for Savings): ";
-        cin >> accountType;
+        while (!(cin >> accountType) || (accountType != 0 && accountType != 1)) {
+            cout << "Invalid input. Please enter 0 for Checking or 1 for Savings: ";
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
         cout << "Enter Your credit Score: ";
-        cin >> credit_score;
+        while (!(cin >> credit_score) || credit_score < 0 || credit_score > 850) {
+            cout << "Invalid input. What is your credit score?: ";
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
         cout << "Enter initial deposit amount: $";
-        cin >> deposit;
+        while (!(cin >> deposit) || deposit < 0) {
+            cout << "Invalid input. Please enter a positive number: $";
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
 
         newmembers[i].set_ac_name(First, last);
         newmembers[i].set_ac_type(accountType);
         newmembers[i].set_ac_balance(deposit);
         newmembers[i].set_ac_interest_rate(credit_score);
     }
+
     cout << "\nAccount Summaries:\n";
     cout << fixed << setprecision(2);
     for (int i = 0; i < 10; i++) {
