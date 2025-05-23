9b22b44199a807d22e03b69a2a1bc0c23ee3917e
Randy <reaston@my365.bellevue.edu>
2025-05-23 04:29:10 -0400

    Mod 12 Assignment updated

--- Module 12/Ch14Exercise01.cpp
+++ Module 12/Ch14Exercise01.cpp
@@ -7,11 +7,21 @@
 #include <stdexcept>
 
 using namespace std;
+class negativeNumber : public exception {
+    const char* what() const noexcept override {
+        return "Number cannot be negative.";
+    }
+};
 
+class nonNumber : public exception {
+    const char* what() const noexcept override {
+        return "Input must be a numeric value.";
+    }
+};
 template <typename T>
 double inches_to_centimeters(T inches) {
     if (inches < 0) {
-        throw invalid_argument("Number cannot be negative");
+        throw negativeNumber();
     }
 
     return inches * 2.54;
@@ -23,35 +33,41 @@ int main() {
     double cent = 0;
     cout << "Welcome and Thank you for using the Converter" << endl;
     cout << "We Will convert length in feet and inches to centimeters" <<endl;
-    cout << "First, enter the length in feet *If you only have inches enter ""0"" for feet)" << endl;
-    while (true) {
-        if (!(cin >> input)) {
-            cerr << "Invalid input. Please enter a numeric value.\n";
-            cin.clear();
-            cin.ignore(10000, '\n');
-            continue;
-        }
-        break;
-    }
-    inches = input*12;
-    cout << "Enter the length in inches" << endl;
-    while (true) {
-        if (!(cin >> input)) {
-            cerr << "Invalid input. Please enter a numeric value.\n";
-            cin.clear();
-            cin.ignore(10000, '\n');
-            continue;
-        }
-        else {
-            try {
-                inches += input;
-                cent = inches_to_centimeters(inches);
-                cout << fixed << setprecision(2) << cent << " cm" << endl;
-                break;
-            } catch (const exception& e) {
-                cerr << "Error: " << e.what() << endl;
 
+    while (true) {
+        try {
+            cout << "First, enter the length in feet *If you only have inches enter ""0"" for feet)" << endl;
+            if (!(cin >> input)) {
+                cin.clear();
+                cin.ignore(10000, '\n');
+                throw nonNumber();
             }
+            if (input < 0) throw negativeNumber();
+            inches = input * 12;
+            break;
+        } catch (const exception& e) {
+            cerr << "Error: " << e.what() << endl;
+        }
+    }
+
+    while (true) {
+        try {
+            cout << "Enter the length in inches" << endl;
+            if (!(cin >> input)) {
+                cin.clear();
+                cin.ignore(10000, '\n');
+                throw nonNumber();
+            }
+            if (input < 0) {
+                throw negativeNumber();
+            }
+            inches += input;
+            cent = inches_to_centimeters(inches);
+            cout << fixed << setprecision(2) << cent << " cm" << endl;
+            break;
+        }
+        catch (const exception& e) {
+            cerr << "Error: " << e.what() << endl;
         }
     }
 
